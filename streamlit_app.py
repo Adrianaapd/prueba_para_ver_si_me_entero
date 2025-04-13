80eb84e108fb8a3d662bf47ba05783542d1b5c25
Adrianaapd <puceiroadriana@gmail.com>
2025-04-13 09:30:09 +0000

    upload 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Mi primera web app")
+st.title("¡Hola, mundo! Esta es una app hecha con Streamlit.")
+
